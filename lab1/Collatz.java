5498a27f3177fd0fec4997680a21d7e4434f60d0
xas-111 <gdesi2022@163.com>
2022-10-15 11:22:25 -0700

    fixed bug with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,7 +2,7 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-/**just testing what happened*/
+/**fixed bug of implementation of nextNumber*/
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
         if (n  == 128) {
@@ -10,7 +10,7 @@ public class Collatz {
         } else if (n == 5) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return n / 2;
         }
     }
 
